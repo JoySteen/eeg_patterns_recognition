b2d865d076c0dfe69c7a1c474df70a4f2b360ab2
Сергей Карбовский <kserx05@rambler.ru>
2016-02-07 02:11:45 +0300

    Добавлен скрипт, который выделяет эпохи из модели сигнала

---
+++ IMK/generator/identify_epochs.m
@@ -0,0 +1,14 @@
+function [epochs] = identify_epochs(signal, Fs, epoch_duration)
+
+    start_time = 0.3;
+    flick_time = 0.15;
+    idle_time = 0.15;
+    
+    N = Fs * epoch_duration;
+    epochs = [];
+    
+    for i = Fs*start_time+N:N:size(signal, 2)
+        epochs = [epochs; signal(:, i-N+1:i)];
+    end
+    
+end
